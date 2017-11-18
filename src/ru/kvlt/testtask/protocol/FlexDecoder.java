fec4035025f34a31091dd6ea4f1444de9e2fc1c5
kvlt <remillary@gmail.com>
2017-11-18 18:42:06 +0700

    getting packet size

--- src/ru/kvlt/testtask/protocol/FlexDecoder.java
+++ src/ru/kvlt/testtask/protocol/FlexDecoder.java
@@ -6,11 +6,14 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import ru.kvlt.testtask.utils.Log;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class FlexDecoder extends ByteToMessageDecoder {
 
-    private static final int MAX_BYTES = 65551;
+    private static final int MAX_BYTES = 65535;
     private static final int MIN_BYTES = 16;
 
     @Override
@@ -21,15 +24,25 @@ public class FlexDecoder extends ByteToMessageDecoder {
             return;
         }
 
-        ByteBuf head = byteBuf.slice(0, 15);
+        ByteBuf head = byteBuf.slice(0, 16);
         ByteBuf body = byteBuf.readSlice(16);
+
+        byte[] sizeBytes = ByteBufUtil.getBytes(byteBuf.slice(12, 14));
+        int size = ((sizeBytes[0] << 8) & 0x0000ff00) | (sizeBytes[1] & 0x000000ff);
+
+        System.out.println(size);
 
         String headStr = ByteBufUtil.prettyHexDump(head);
         String bodyStr = ByteBufUtil.prettyHexDump(body);
 
-        Log.print("<HEAD>\n" + headStr);
-        Log.print("<BODY>\n" + bodyStr);
-        Log.print("\n");
+        ByteBuf preamble = head.slice(0, 4);
+        String preamleStr = preamble.toString(Charset.forName("UTF-8"));
+        Log.print(preamleStr);
+
+        //Log.print("<HEAD>\n" + headStr);
+        //Log.print("<BODY>\n" + bodyStr);
+        //Log.print("\n");
+
     }
 
 }
