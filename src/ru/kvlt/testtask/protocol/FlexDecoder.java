ac422727f6a03f2602e5427ffe2e8ea1e9ca4d41
kvlt <remillary@gmail.com>
2017-11-18 19:33:01 +0700

    slicing head except checksums

--- src/ru/kvlt/testtask/protocol/FlexDecoder.java
+++ src/ru/kvlt/testtask/protocol/FlexDecoder.java
@@ -14,35 +14,43 @@ import java.util.List;
 public class FlexDecoder extends ByteToMessageDecoder {
 
     private static final int MAX_BYTES = 65535;
-    private static final int MIN_BYTES = 16;
+    private static final int HEAD_SIZE = 16;
+    private static final Charset CHARSET = Charset.forName("UTF-8");
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> list) throws Exception {
-        int length = byteBuf.readableBytes();
-
-        if (length > MAX_BYTES || length < MIN_BYTES) {
+        int packetSize = getPacketSize(byteBuf);
+        if (packetSize != byteBuf.readableBytes()) {
             return;
         }
 
         ByteBuf head = byteBuf.slice(0, 16);
         ByteBuf body = byteBuf.readSlice(16);
 
-        byte[] sizeBytes = ByteBufUtil.getBytes(byteBuf.slice(12, 14));
-        int size = ((sizeBytes[0] << 8) & 0x0000ff00) | (sizeBytes[1] & 0x000000ff);
-
-        System.out.println(size);
-
-        String headStr = ByteBufUtil.prettyHexDump(head);
-        String bodyStr = ByteBufUtil.prettyHexDump(body);
-
         ByteBuf preamble = head.slice(0, 4);
-        String preamleStr = preamble.toString(Charset.forName("UTF-8"));
+        ByteBuf IDr = head.slice(4, 8);
+        ByteBuf IDs = head.slice(8, 12);
+        //TODO: checksum
+        //int headCheckSum = head.getByte(14) & 0xFF;
+
+        String preamleStr = preamble.toString(CHARSET);
         Log.print(preamleStr);
+    }
 
-        //Log.print("<HEAD>\n" + headStr);
-        //Log.print("<BODY>\n" + bodyStr);
-        //Log.print("\n");
+    private int checkSum(ByteBuf buf, int length) {
+//        int tempSum;
+//        int buffer = ByteBufUtil.getBytes(buf).length;
+//
+//        while (length-- > 0) {
+//            tempSum ^= buf++;
+//        }
+    }
 
+    private int getPacketSize(ByteBuf buf) {
+        byte[] sizeBytes = ByteBufUtil.getBytes(buf.slice(12, 14));
+        int bodySize = sizeBytes[0] | sizeBytes[1] << 8;
+
+        return HEAD_SIZE + bodySize;;
     }
 
 }
